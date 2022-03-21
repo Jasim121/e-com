5b29c31572c23bdd7211b3a98f6ff8bacc10af6d
Jasim Gazi <Jasimakram96@gmail.com>
2022-03-21 16:31:20 +0530

    e-comerce

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     anime.timeline({
+//         easing: 'easeOutExpo',
+//       })
+//       .add({
+//         targets:'.navbar-nav a',
+//         translateY:[20, 5],
+//         delay: anime.stagger(300, {easing: 'easeOutQuad'}),
+//         offset:'-=200',
+//         easing: 'linear',
+//          direction: 'alternate',
+//         // translateZ:'50px'
+//     })
+// })
+// window.addEventListener("scroll", ()=>{
+// })
+
+
+// gsap
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".headn",
+// {x:300,
+//   duration:3,
+//   scrollTrigger:{
+//     trigger:".headn",
+//     toggleActions:"restart",
+//     scrub:true,
+//   }
+// })
+  
+
+
+
+
+
+
+
+
+
+
